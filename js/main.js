26e9b64550e2f0abc5e6d030c2880cbb6ef75da6
dhivyasundaram312 <dhivyasundaram312@users.noreply.github.com>
2020-04-28 16:40:44 +0530

    Resume

---
+++ js/main.js
@@ -0,0 +1,38 @@
+jQuery(function($) {'use strict',
+
+// portfolio filter
+	$(window).load(function(){'use strict',
+		$portfolio_selectors = $('.portfolio-filter >li>a');
+		if($portfolio_selectors!='undefined'){
+			$portfolio = $('.portfolio-items');
+			$portfolio.isotope({
+				itemSelector : '.col-sm-3',
+				layoutMode : 'fitRows'
+			});
+			
+			$portfolio_selectors.on('click', function(){
+				$portfolio_selectors.removeClass('active');
+				$(this).addClass('active');
+				var selector = $(this).attr('data-filter');
+				$portfolio.isotope({ filter: selector });
+				return false;
+			});
+		}
+	});
+	
+	$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
+}());
